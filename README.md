a21a8f75f732940205bdb0aae50e12d86eafe216
Zachjrivera <Zachjrivera@gmail.com>
2018-09-12 13:22:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Javagoaldiggersnotes
---
+++ src/main/java/demopackage/Main.java
@@ -0,0 +1,471 @@
+package demopackage;
+
+import javax.sound.midi.Soundbank;
+import javax.xml.transform.sax.SAXSource;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello world");
+//        int myNumber;
+//                myNumber = 88;
+//                System.out.println(myNumber);
+//
+//
+//    String greeting = "Hello";
+//    String name = " Zach";
+//        System.out.println(greeting + name);
+//        int value = 0;
+//
+//        while(value < 10)
+//       {
+//           System.out.println("Hello" + value);
+//           value = value + 1 ;
+//       }
+//
+//       for (int i = 0 ; i < 5 ; i ++)
+//       {
+//
+//           System.out.printf("the value of i is: %d\n", i);
+//
+//       }
+//
+//            int myInt = 20;
+//       if(myInt < 10)
+//       {
+//           System.out.println("Danm stright");
+//
+//
+//       }
+//       else if(myInt > 20)
+//    {
+//        System.out.println("No you wrong");
+//    }
+//
+//        else
+//            {
+//                System.out.println("None");
+//            }
+//
+//
+//        int whoop = 0;
+//
+//        while(true)
+//        {
+//           if(whoop == 5)
+//                {
+//                    break;
+//                }
+//
+//                whoop++;
+//            System.out.println("Running");
+//        }
+        //Create scanner object
+//        Scanner input = new Scanner(System.in);
+//        // output prompt
+//        System.out.println("Enter a your phone number: ");
+//       //wait for line of text from user
+//        int value = input.nextInt();
+//        //String line =input.nextLine();
+//        //tell them what the entered
+//        System.out.println("You entered: " + value);
+
+        //statements go here
+
+        /*
+        System.out.println("Hello World");
+         System.out.println("Zachary");
+
+        // types int short long byte float double char boolean   /n goes to next line /t tabs in line
+        int myInt = 100;
+        short myShort = 2000;
+        long myLong = 101L;
+        byte myByte = 50;
+        float myFloat = 2.4f;
+        double myDouble = 6.7;
+        // single ' char " string
+        char myChar = 'f';
+        boolean myBool = true;
+        String favArtist = "My fav artist is \t Biggie";
+        int number = 2, otherNumber = 2;
+        // final is constant can not change
+        final int MYCONSTANTINT = 420;
+
+            System.out.println(Math.pow(2,3));
+            System.out.println(myBool);
+            System.out.println(favArtist);
+            System.out.println(myDouble);
+            System.out.println(myInt);
+        */
+
+//
+//        byte num1 = 10;
+//        long converted = num1; // how to convert
+//
+//        //narrowing
+//
+//        long bigLongData = 100l;
+//        byte smallByte = (byte) bigLongData; // () is a cast to show that i want it to store a byte will result in f'ed up numbers
+
+//        int x = 21;
+//
+//        System.out.println(x > 19 ? "She can get it" : "Nah Son");
+//
+//        // int is primitive Integer is a class class can have methods
+//        String  name = "Zachary";
+//        System.out.println(name.length());
+//
+//
+//        String  greeting = "hello";
+//        String s = greeting.substring(0,3) + "p";
+//        System.out.println(s);
+//
+//        String d = "Fun";
+//        String y = "fun";
+//
+//
+//        System.out.println(d.equalsIgnoreCase(y));
+//
+
+
+//
+//
+
+//        int myInt = 100;
+//             System.out.println(myInt);
+//
+//        short myShort = 2000;
+//
+//             System.out.println(myShort);
+//
+//        long myLong = 101L;
+//
+//             System.out.println(myLong);
+//
+//        byte myByte = 50;
+//
+//             System.out.println(myByte);
+//
+//        float myFloat = 2.4f;
+//
+//             System.out.println(myFloat);
+//
+//        double myDouble = 6.7;
+//
+//             System.out.println(myDouble);
+//
+//        char myChar = 'f';
+//
+//             System.out.println(myChar);
+//
+//        boolean myBool = true;
+//
+//             System.out.println(myBool);
+//        // this is a method String hand = "Left";
+//        String favoriteFood = "pizza";
+//        System.out.println(favoriteFood.charAt(3));
+//        System.out.println(favoriteFood.length());
+
+
+        //homework
+        /*
+        System.out.println("Hello world");
+        int myNumber;
+                myNumber = 88;
+                System.out.println(myNumber);
+
+
+    String greeting = "Hello";
+    String name = " Zach";
+        System.out.println(greeting + name);
+      */
+      /*
+        int value = 0;
+
+        while(value < 10)
+       {
+           System.out.println("Hello" + value);
+           value = value + 1 ;
+       }
+
+       for (int i = 0 ; i < 5 ; i ++)
+       {
+
+           System.out.printf("the value of i is: %d\n", i);
+
+       }
+
+            int myInt = 20;
+       if(myInt < 10)
+       {
+           System.out.println("Danm stright");
+
+
+       }
+       else if(myInt > 20)
+    {
+        System.out.println("No you wrong");
+    }
+
+        else
+            {
+                System.out.println("None");
+            }
+
+
+        int whoop = 0;
+
+        while(true)
+        {
+           if(whoop == 5)
+                {
+                    break;
+                }
+
+                whoop++;
+            System.out.println("Running");
+        }
+      */
+//
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter a your phone number: ");
+//
+//        int value = input.nextInt();
+//
+//        System.out.println("You entered: " + value);
+
+
+//        String name = String.join ("," , "Zach" , "Rivera", "Sumo");
+//        System.out.println(name);
+
+//        String firstName= "Zach";
+//        String lastName = "Rivera";
+//        String nickName = " is Sumo";
+//        String delim = " ";
+//        String fullName = String.join(delim  , firstName , lastName , nickName );
+//        System.out.println(fullName);
+
+//
+//    String name = " ";
+//    if (name != null)
+//    {
+//        System.out.println("Fuckyou");
+//    }else
+//        {
+//            System.out.println("Hi Buddy");
+        // }
+
+//        StringBuilder builder = new StringBuilder();
+//
+//        builder.append("Mr").append(" Fucking");// this is a chain
+//        builder.append(" Lover");
+//        builder.append(" Lover");
+//        System.out.println(builder);
+
+//        Scanner input = new Scanner (System.in); // this allows input to be typed in
+//        System.out.println(" aye girl whats your name");
+//        String name = input.nextLine();
+//        System.out.println("That's a shitty name " + name );
+//        System.out.println("Whats your number");
+//        long number = input.nextLong();
+//        System.out.println("Bet you DTF?");
+//        Boolean dtf = input.nextBoolean();
+//
+//        if(dtf != true)
+//        {
+//            System.out.println("Maybe later?");
+//        } else
+//            {
+//                System.out.println("AWWWWW SHIT");
+//            }
+//
+//        Scanner math = new Scanner(System.in);
+//        System.out.println("Give me a  number");
+//        long num1 = math.nextLong();
+//        System.out.println("And a second number ?");
+//        long num2 = math.nextLong();
+//
+//// System.out.println("the sum of " + num1 + " + " + num2 +" is " + (num1+num2));
+////;
+////        System.out.println("The sum of " + num1 + " * " + num2 + " is " + (num1*num2));
+////
+////        System.out.println("The remainder of " + num1 + " % " + num2 + " is " + (num1 % num2) );
+//
+//        System.out.println("choose a sign + * % ");
+//        String sign = math.next();
+//
+//        if( sign.equals("+") )
+//        {
+//            System.out.println("the sum of " + num1 + " + " + num2 +" is " + (num1+num2));
+//        }  else if (sign.equals("*"))
+//            {
+//                System.out.println("The sum of " + num1 + " * " + num2 + " is " + (num1*num2));
+//            }  else if (sign.equals("%"))
+//            {
+//                System.out.println("The remainder of " + num1 + " % " + num2 + " is " + (num1 % num2));
+//            }
+
+
+//        Scanner value = new Scanner(System.in);
+//
+//        System.out.println("Enter a your phone number: ");
+//
+//        String value = input.nextInt();
+//
+//        System.out.println("You entered: " + value);
+//        double x = 10000.0/ 3.0;
+//        System.out.println(x+"\n");
+//        System.out.printf("%8.2f", x);
+
+
+        //System.out.println("Hello %s,%s,%s,num1 mu"); this will run the scanner thing faster
+//string number and experiment with 3
+
+
+//        System.out.printf("%15s","Tired" +"\n");
+//
+//        System.out.printf( "%06d" , 22);
+//
+//        System.out.printf("\n" + "%7.3f" , 99.9 );
+
+//        int x = 0;
+//
+//        while(x < 11)
+//        {
+//
+//            System.out.println("The value of x is " + x );
+//            x = x +1;
+//            //break;
+//        }
+
+//goes by two
+
+//        int num = 10;
+//                while(num >= 0 )
+//                {
+//                    System.out.println(num);
+//                    num = num - 2;
+//                }
+//
+//
+//   }
+
+//        //not use too much
+//        int i = 0;
+//        System.out.println("welcome to my website");
+//        do {
+//            System.out.println(" Incorrect password - Try agian");
+//
+//            System.out.println("Trial #" + i);
+//            i++;
+//            if (i == 4) {
+//                System.out.println("you are locked out");
+//            }
+//        } while (i < 4);{
+//    }
+
+//Create a program that prompts a user for their name
+//If the users name is Bob or Alice.
+//Print out You are now signed in Bob or Alice
+//If the user enters a different name keep asking the user for their
+        int tries = 0;
+        String b = "bob";
+        String a = "alice";
+        String name = "";
+        Scanner login = new Scanner(System.in);
+        System.out.println("Please log in as bob or alice");
+      // String signIn = login.nextLine();
+
+       // do(!)
+//            {
+//                System.out.println("Wrong Name.");
+//            }
+        while(!name.equalsIgnoreCase("Bob")){
+
+            tries++;
+           
+            String signIn = login.nextLine();
+
+            if (signIn.equalsIgnoreCase(b))
+                    {
+                        System.out.println("Welcome Bob you are now signed in it took you " + tries + " tries");
+                            break;
+                    }
+                        else if (signIn.equalsIgnoreCase(a))
+                        {
+                            System.out.println("Welcome Alice you are now signed in it took you" + tries + "tries");
+                                break;
+                        }
+                            else
+                                {
+                                    System.out.println("Wrong name");
+
+                                }
+
+
+
+        }
+
+
+
+
+//        while (!signIn.equalsIgnoreCase(b))
+//
+//        {
+//
+//
+//
+//                System.out.println("Try again");
+//
+//
+//
+//                    if (signIn.equalsIgnoreCase(b))
+//                    {
+//                        System.out.println("Welcome Bob you are now signed in");
+//
+//                    }
+//                        else if (signIn.equalsIgnoreCase(a))
+//                        {
+//                            System.out.println("Welcome Alice you are now signed in");
+//                        }
+//                // else {
+//                    //System.out.println("Please try again");
+//               // }
+//
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
